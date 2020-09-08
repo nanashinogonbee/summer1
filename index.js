95bfb16ed691f18ed429dd9a97e541fcaf7b5736
Ilya Goss <goss@corp.ifmo.ru>
2020-09-08 16:13:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function summer(r, sum) {
       if ('cors' in r.query) r.res.set(CORS);
       r.res.format({
         'text/html': () => r.res.send(`<h2>Сумма:<i>${sum}</i></h2>`),
-        'application/json': () => r.res.json({"Сумма:": sum})
+        'application/json': () => r.res.json({"Результат:": sum})
       });  
 }
 Router
